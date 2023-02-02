842446217e984571d44050f4c9645aa8950d180b
delichka <delya.pavlova@gmail.com>
2023-02-02 12:32:19 +0000

    Showind the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemp(response) {
+  let tempElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  let descrElement = document.querySelector("#descr");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descrElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "53d797bcb4bc754e1d07eafabdcafce8";
+let city = "London";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemp);

9f0ac5a0f4cb4fbe76e5047a7a92ab27eefc3f3f
delichka <delya.pavlova@gmail.com>
2023-02-08 21:51:33 +0000

    Created list of forecast in Java

--- src/app.js
+++ src/app.js
@@ -21,6 +21,33 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` 
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/01n@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temp">
+                  <span class="temp-max">18°</span>
+                  <span class="temp-min">12°</span>
+                </div>
+              </div>
+            </div> `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemp(response) {
   let tempElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
@@ -78,6 +105,8 @@ function showCelsTemp(event) {
 let city = "London";
 
 let celsiusTemp = null;
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
